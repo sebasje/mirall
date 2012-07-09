3d0dd25c75eb3714c85d79ffc062e01d206bd257
Sebastian Kügler <sebas@kde.org>
2012-07-10 01:55:51 +0200

    empty Plasmoid
    This will become the status controller for the panel.

---
+++ src/plasma/applet/owncloudapplet.cpp
@@ -0,0 +1,69 @@
+/***************************************************************************
+ *   Copyright 2012 by Sebastian Kügler <sebas@kde.org>                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+#include "owncloudapplet.h"
+
+#include <KDebug>
+#include <KConfigDialog>
+#include <KConfigGroup>
+
+#include <Plasma/DeclarativeWidget>
+
+OwncloudApplet::OwncloudApplet(QObject *parent, const QVariantList &args)
+    : Plasma::PopupApplet(parent, args),
+    m_declarativeWidget(0)
+{
+    setAspectRatioMode(Plasma::IgnoreAspectRatio);
+//    setCacheMode(DeviceCoordinateCache);
+}
+
+OwncloudApplet::~OwncloudApplet()
+{
+}
+
+void OwncloudApplet::init()
+{
+    setPopupIcon("view-pim-calendar");
+    m_declarativeWidget = new Plasma::DeclarativeWidget(this);
+    m_declarativeWidget->setMinimumSize(220, 250);
+    configChanged();
+}
+
+void OwncloudApplet::configChanged()
+{
+    // ...
+}
+
+QGraphicsWidget *OwncloudApplet::graphicsWidget()
+{
+    return m_declarativeWidget;
+}
+
+void OwncloudApplet::configAccepted()
+{
+}
+
+void OwncloudApplet::createConfigurationInterface(KConfigDialog *parent)
+{
+    parent->setButtons( KDialog::Ok | KDialog::Cancel | KDialog::Apply );
+    connect(parent, SIGNAL(applyClicked()), this, SLOT(configAccepted()));
+    connect(parent, SIGNAL(okClicked()), this, SLOT(configAccepted()));
+}
+
+#include "owncloudapplet.moc"
---
+++ src/plasma/applet/owncloudapplet.h
@@ -0,0 +1,55 @@
+/***************************************************************************
+ *   Copyright 2012 by Sebastian Kügler <sebas@kde.org>                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+#ifndef OWNCLOUDAPPLET_H
+#define OWNCLOUDAPPLET_H
+
+#include <Plasma/PopupApplet>
+#include <Plasma/Label>
+
+namespace Plasma {
+    class DeclarativeWidget;
+}
+
+class OwncloudApplet : public Plasma::PopupApplet
+{
+    Q_OBJECT
+    public:
+        OwncloudApplet(QObject *parent, const QVariantList &args);
+        ~OwncloudApplet();
+
+        void init();
+        QGraphicsWidget *graphicsWidget();
+
+    public Q_SLOTS:
+        void configChanged();
+
+    protected:
+        void createConfigurationInterface(KConfigDialog *parent);
+
+    protected Q_SLOTS:
+        void configAccepted();
+
+    private:
+        Plasma::DeclarativeWidget *m_declarativeWidget;
+};
+
+K_EXPORT_PLASMA_APPLET(owncloud, OwncloudApplet)
+
+#endif
