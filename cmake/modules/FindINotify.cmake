60bd9547964f466a67fdae5fbefb23f7e60526d2
Sebastian Kügler <sebas@kde.org>
2012-12-30 03:04:00 +0100

    clean up findinotify.cmake

--- cmake/modules/FindINotify.cmake
+++ cmake/modules/FindINotify.cmake
@@ -4,22 +4,10 @@
 # also defined, but not for general use are
 #  INOTIFY_LIBRARY, where to find the inotify library.
 
-#<<<<<<< HEAD
-##find_path(INOTIFY_INCLUDE_DIR sys/inotify.h ${CMAKE_ARCH_TRIPLET}/sys/inotify.h)
-#find_path(INOTIFY_INCLUDE_DIR NAMES sys/inotify.h ${CMAKE_LIBRARY_ARCHITECTURE}/sys/inotify.h)
-##find_path(INOTIFY_INCLUDE_DIR NAMES sys/inotify.h ${CMAKE_ARCH_TRIPLET}/sys/inotify.h x86_64-linux-gnu/sys/inotify.h)
-#mark_as_advanced(INOTIFY_INCLUDE_DIR)
-
-##set(INOTIFY_INCLUDE_DIR "/usr/include/x86_64-linux-gnu/")
-##set(INOTIFY_FOUND TRUE)
-
-## handle the QUIETLY and REQUIRED arguments and set INOTIFY_FOUND to TRUE if
-#=======
 find_path(INOTIFY_INCLUDE_DIR sys/inotify.h 
           HINTS /usr/include/${CMAKE_LIBRARY_ARCHITECTURE})
 mark_as_advanced(INOTIFY_INCLUDE_DIR)
 
-#>>>>>>> master
 # all listed variables are TRUE
 # handle the QUIETLY and REQUIRED arguments and set INOTIFY_FOUND to TRUE if
 include(FindPackageHandleStandardArgs)
